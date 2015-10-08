92a9a2e554f4ce4255dc8205864f5fa14f1424f7
Amor Dimaano <amor.dimaano@obf.ateneo.edu>
2015-10-08 16:54:33 +0800

    Problem #2
    Problem started.
    Initialized pseudo-code.

---
+++ Problem #2.py
@@ -0,0 +1,11 @@
+__author__ = 'amordimaano'
+
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the
+# even-valued terms.
+
+def main():
+    # Generate Fibonacci sequence
+    # Find even numbers
+    # Get sum of even numbers
+
+main()
