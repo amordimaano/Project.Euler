35f611b00e6b4431943648f2ac40762103208542
Amor Dimaano <amor.dimaano@obf.ateneo.edu>
2015-10-10 00:25:41 +0800

    Problem #2
    Solved

--- Problem #2.py
+++ Problem #2.py
@@ -5,7 +5,22 @@ __author__ = 'amordimaano'
 
 def main():
     # Generate Fibonacci sequence
+    fibonacci = [1,1]
+    while fibonacci[-1] < 4000000 and fibonacci[-1] + fibonacci[-2] < 4000000 :
+        fibonacci.append(fibonacci[-2] + fibonacci[-1])
+
     # Find even numbers
-    # Get sum of even numbers
+    even_numbers = []
+    for i in fibonacci:
+        if i % 2 == 0:
+            even_numbers.append(i)
+
+    #  Get sum of even numbers
+    sum_even_numbers = 0
+    for i in even_numbers:
+        sum_even_numbers = sum_even_numbers + i
+
+    # Answer
+    print sum_even_numbers
 
 main()
