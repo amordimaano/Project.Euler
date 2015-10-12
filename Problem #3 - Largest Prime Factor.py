5c39ffeacaab77f3ff3f5cdc34aaaec23269a307
Amor Dimaano <amor.dimaano@obf.ateneo.edu>
2015-10-12 23:49:59 +0800

    Problem #3
    Trial. Not Successful.

--- Problem #3 - Largest Prime Factor.py
+++ Problem #3 - Largest Prime Factor.py
@@ -3,3 +3,18 @@ __author__ = 'amordimaano'
 
 # What is the largest prime factor of the number 600851475143?
 
+def main():
+    # Process: divide by smallest prime then keep dividing until you get a prime. Keep repeating.
+
+    original_number = 600851475143
+
+    prime_list = []
+
+    for number in range(original_number):
+        for prime in prime_list:
+            if number % prime == 0:
+                
+
+    # Get answer.
+
+main()
