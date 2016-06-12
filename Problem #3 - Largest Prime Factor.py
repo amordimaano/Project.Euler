f513ca7877c235ca262c59c13692918f8017548b
Amor Dimaano <amor.dimaano@obf.ateneo.edu>
2016-06-12 11:13:39 -0700

    STARTING AGAIN

--- Problem #3 - Largest Prime Factor.py
+++ Problem #3 - Largest Prime Factor.py
@@ -7,13 +7,11 @@ def main():
     # Process: divide by smallest prime then keep dividing until you get a prime. Keep repeating.
 
     original_number = 600851475143
-
+    new_number = 0
     prime_list = []
 
-    for number in range(original_number):
-        for prime in prime_list:
-            if number % prime == 0:
-                
+
+
 
     # Get answer.
 
